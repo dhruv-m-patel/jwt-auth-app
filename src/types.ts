1a275454db3075a59438c8eb1a8f8e24a3226b18
Dhruv Patel <dhruv.codelab@gmail.com>
2021-12-27 22:37:15 -0600

    feat: setup mobx prerequisites to work with state management

---
+++ src/types.ts
@@ -0,0 +1,8 @@
+export interface Tokens {
+    accessToken: string;
+    accessTokenExpiresIn: number;
+    accessTokenExpiry: string;
+    refreshToken: string;
+    refreshTokenExpiresIn: number;
+    refreshTokenExpiry: string;
+}
